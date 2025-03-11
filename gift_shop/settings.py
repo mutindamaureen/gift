b16b4504ab41867cc504dd0f1a38a28b871d2ff1
Maureen <119786119+mutindamaureen@users.noreply.github.com>
2025-03-12 01:56:46 +0300

    Update settings.py

--- gift_shop/settings.py
+++ gift_shop/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media/'
 
