483be99a4f9e9b77b983385937c8d7c99255356b
Maureen <119786119+mutindamaureen@users.noreply.github.com>
2025-03-12 12:06:18 +0300

    Update settings.py

--- gift_shop/settings.py
+++ gift_shop/settings.py
@@ -140,7 +140,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')  # If you have extra static files
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
